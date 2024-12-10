119f455c1e2e52cf9686aaecd0ea2f027f738046
taemin <jungtaemin1214@naver.com>
2024-12-10 16:25:55 +0900

    2024-12-10

---
+++ src/main/java/com/example/springbootdeveloper/service/BlogService.java
@@ -0,0 +1,49 @@
+package com.example.springbootdeveloper.service;
+
+import com.example.springbootdeveloper.domain.Article;
+import com.example.springbootdeveloper.dto.AddArticleRequest;
+import com.example.springbootdeveloper.dto.UpdateArticleRequest;
+import com.example.springbootdeveloper.repository.BlogRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class BlogService {
+
+    private final BlogRepository blogRepository;
+
+    // 블로그 글 추가 메서드
+    public Article save(AddArticleRequest request) {
+        return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
+    }
+
+    public Article findById(long id) {
+        return blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+    }
+
+    public void delete(long id) {
+        blogRepository.deleteById(id);
+    }
+
+    @Transactional
+    public Article update(long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
+    }
+}
